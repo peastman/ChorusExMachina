336c3a4813380f252689f2051e07c3bbf117f3f2
Peter Eastman <peastman@stanford.edu>
2024-12-12 16:46:12 -0800

    Midi input

--- src/director.rs
+++ src/director.rs
@@ -5,7 +5,9 @@ use std::sync::mpsc;
 
 pub enum Message {
     NoteOn {syllable: String, note_index: i32, velocity: f32},
-    NoteOff
+    NoteOff,
+    SetVolume {volume: f32},
+    SetPitchBend {semitones: f32}
 }
 
 struct Transition {
@@ -15,7 +17,7 @@ struct Transition {
 }
 
 enum TransitionData {
-    VolumeChange {start_volume: f32, end_volume: f32},
+    EnvelopeChange {start_envelope: f32, end_envelope: f32},
     ShapeChange {start_shape: Vec<f32>, end_shape: Vec<f32>, start_nasal_coupling: f32, end_nasal_coupling: f32},
     FrequencyChange {start_frequency: f32, end_frequency: f32}
 }
@@ -33,9 +35,13 @@ pub struct Director {
     step: i64,
     transitions: Vec<Transition>,
     current_note: Option<Note>,
+    volume: f32,
+    envelope: f32,
+    frequency: f32,
+    bend: f32,
     shape_after_transitions: Vec<f32>,
     nasal_coupling_after_transitions: f32,
-    volume_after_transitions: f32,
+    envelope_after_transitions: f32,
     frequency_after_transitions: f32,
     message_receiver: mpsc::Receiver<Message>
 }
@@ -48,9 +54,13 @@ impl Director {
             step: 0,
             transitions: vec![],
             current_note: None,
+            volume: 1.0,
+            envelope: 0.0,
+            frequency: 1.0,
+            bend: 1.0,
             shape_after_transitions: vec![0.0; 44],
             nasal_coupling_after_transitions: 0.0,
-            volume_after_transitions: 0.0,
+            envelope_after_transitions: 0.0,
             frequency_after_transitions: 0.0,
             message_receiver: message_receiver
         }
@@ -71,14 +81,15 @@ impl Director {
         }
         else {
             self.add_transition(delay, 0, TransitionData::FrequencyChange {start_frequency: frequency, end_frequency: frequency});
-            self.add_transition(delay, 5000, TransitionData::VolumeChange {start_volume: self.volume_after_transitions, end_volume: 1.0});
+            self.add_transition(delay, 2000, TransitionData::EnvelopeChange {start_envelope: self.envelope_after_transitions, end_envelope: 1.0});
         }
         for c in &new_syllable.initial_vowels {
             delay = self.add_transient_vowel(delay, *c);
         }
         let shape = self.phonemes.get_vowel_shape(new_syllable.main_vowel).unwrap();
         let nasal_coupling = self.phonemes.get_nasal_coupling(new_syllable.main_vowel);
-        self.add_transition(delay, 7000, TransitionData::ShapeChange {
+        let transition_time = if self.current_note.is_some() || new_syllable.initial_vowels.len() > 0 {7000} else {0};
+        self.add_transition(delay, transition_time, TransitionData::ShapeChange {
             start_shape: self.shape_after_transitions.clone(),
             end_shape: shape.clone(),
             start_nasal_coupling: self.nasal_coupling_after_transitions,
@@ -104,7 +115,7 @@ impl Director {
                 delay = self.add_transient_vowel(delay, *c);
             }
         }
-        self.add_transition(delay, 3000, TransitionData::VolumeChange {start_volume: self.volume_after_transitions, end_volume: 0.0});
+        self.add_transition(delay, 3000, TransitionData::EnvelopeChange {start_envelope: self.envelope_after_transitions, end_envelope: 0.0});
         self.current_note = None;
     }
 
@@ -123,8 +134,8 @@ impl Director {
     fn add_transition(&mut self, delay: i64, duration: i64, data: TransitionData) {
         let transition = Transition { start: self.step+delay, end: self.step+delay+duration, data: data };
         match &transition.data {
-            TransitionData::VolumeChange {start_volume, end_volume} => {
-                self.volume_after_transitions = *end_volume;
+            TransitionData::EnvelopeChange {start_envelope, end_envelope} => {
+                self.envelope_after_transitions = *end_envelope;
             }
             TransitionData::ShapeChange {start_shape, end_shape, start_nasal_coupling, end_nasal_coupling} => {
                 self.shape_after_transitions = end_shape.clone();
@@ -161,6 +172,14 @@ impl Director {
                         Message::NoteOff => {
                             self.note_off();
                         }
+                        Message::SetVolume {volume} => {
+                            self.volume = volume;
+                            self.update_volume();
+                        }
+                        Message::SetPitchBend {semitones} => {
+                            self.bend = f32::powf(2.0, semitones as f32/12.0);
+                            self.update_frequency();
+                        }
                     }
                 }
                 Err(_) => {
@@ -171,16 +190,16 @@ impl Director {
     }
 
     fn update_transitions(&mut self) {
-        for transition in &self.transitions {
+        for i in 0..self.transitions.len() {
+            let transition = &self.transitions[i];
             if self.step >= transition.start {
                 let fraction = (self.step-transition.start) as f32 / (transition.end-transition.start) as f32;
                 let weight2 = if self.step < transition.end {0.5-0.5*(fraction*std::f32::consts::PI).cos()} else {1.0};
                 let weight1 = 1.0-weight2;
                 match &transition.data {
-                    TransitionData::VolumeChange {start_volume, end_volume} => {
-                        for voice in &mut self.voices {
-                            voice.set_volume(weight1*start_volume + weight2*end_volume);
-                        }
+                    TransitionData::EnvelopeChange {start_envelope, end_envelope} => {
+                        self.envelope = weight1*start_envelope + weight2*end_envelope;
+                        self.update_volume();
                     }
                     TransitionData::ShapeChange {start_shape, end_shape, start_nasal_coupling, end_nasal_coupling} => {
                         for voice in &mut self.voices {
@@ -193,13 +212,24 @@ impl Director {
                         }
                     }
                     TransitionData::FrequencyChange {start_frequency, end_frequency} => {
-                        for voice in &mut self.voices {
-                            voice.set_frequency(weight1*start_frequency + weight2*end_frequency);
-                        }
+                        self.frequency = weight1*start_frequency + weight2*end_frequency;
+                        self.update_frequency();
                     }
                 }
             }
         }
         self.transitions.retain(|t| self.step < t.end);
+    }
+
+    fn update_volume(&mut self) {
+        for voice in &mut self.voices {
+            voice.set_volume(self.volume*self.envelope);
+        }
+    }
+
+    fn update_frequency(&mut self) {
+        for voice in &mut self.voices {
+            voice.set_frequency(self.frequency*self.bend);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use chorus::phonemes::Phonemes;
 use chorus::director::{Director, Message};
 
 use rodio::{OutputStream, Source};
+use midir::MidiInput;
 use std::sync::mpsc;
 use std::thread::sleep;
 use std::time::Duration;
@@ -37,15 +38,42 @@ impl Source for Player {
     }
 }
 
+struct MidiController {
+    sender: mpsc::Sender<Message>,
+    last_note: u8
+}
+
+fn process_midi_message(timestamp: u64, message: &[u8], controller: &mut MidiController) {
+    if message[0] == 144 {
+        controller.sender.send(Message::NoteOn {syllable: "U".to_string(), note_index: message[1] as i32, velocity: message[2] as f32 / 127.0});
+        controller.last_note = message[1];
+    }
+    else if message[0] == 128 && controller.last_note == message[1] {
+        controller.sender.send(Message::NoteOff);
+    }
+    else if message[0] == 176 && message[1] == 1 {
+        controller.sender.send(Message::SetVolume {volume: message[2] as f32 / 127.0});
+    }
+    else if message[0] == 224 {
+        let value = message[2] as f32 + message[1] as f32 / 127.0;
+        controller.sender.send(Message::SetPitchBend {semitones: (value-64.0)/32.0});
+    }
+}
+
 fn main() {
     let (sender, receiver) = mpsc::channel();
     let mut player = Player { director: Director::new(vec![voice::Voice::new(48000)], receiver) };
     let (_stream, handle) = OutputStream::try_default().unwrap();
     let _result = handle.play_raw(player.convert_samples());
-    sender.send(Message::NoteOn {syllable: "Ai".to_string(), note_index: 55, velocity: 1.0});
-    sleep(Duration::from_millis(2000));
-    sender.send(Message::NoteOn {syllable: "o".to_string(), note_index: 60, velocity: 1.0});
-    sleep(Duration::from_millis(2000));
-    sender.send(Message::NoteOff);
-    sleep(Duration::from_millis(10000));
+
+    let mut midi_in = MidiInput::new("Chorus").unwrap();
+    let in_ports = midi_in.ports();
+    let in_port = &in_ports[1];
+    let _conn_in = midi_in.connect(
+        in_port,
+        "midir-read-input",
+        process_midi_message,
+        MidiController {sender: sender, last_note: 255},
+    ).unwrap();
+    sleep(Duration::from_millis(100000));
 }
