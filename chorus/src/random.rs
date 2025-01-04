ba53a87176584ee3579245955c721790593143fb
Peter Eastman <peastman@stanford.edu>
2025-01-04 13:37:49 -0800

    Added license header to source files

--- chorus/src/random.rs
+++ chorus/src/random.rs
@@ -1,3 +1,18 @@
+// Copyright 2025 by Peter Eastman
+//
+// This file is part of Chorus Ex Machina.
+//
+// Chorus Ex Machina is free software: you can redistribute it and/or modify it under the terms
+// of the GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// Chorus Ex Machina is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+// the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with Chorus Ex Machina.
+// If not, see <https://www.gnu.org/licenses/>.
+
 use std::time::{SystemTime, UNIX_EPOCH};
 
 const UNIFORM_SCALE: f32 = 1.0/(0x100000000i64 as f32);
--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -1,3 +1,18 @@
+// Copyright 2025 by Peter Eastman
+//
+// This file is part of Chorus Ex Machina.
+//
+// Chorus Ex Machina is free software: you can redistribute it and/or modify it under the terms
+// of the GNU Lesser General Public License as published by the Free Software Foundation, either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// Chorus Ex Machina is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+// the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License along with Chorus Ex Machina.
+// If not, see <https://www.gnu.org/licenses/>.
+
 mod editor;
 
 use chorus;
